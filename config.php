f34f490688a80beddf2d9f87df2799764b81edfd
mdmassa <djsheepwolfbr@gmail.com>
2023-02-26 17:35:09 -0300

    Updating php settings

--- config.php
+++ config.php
@@ -1,45 +1,36 @@
 <?php
+    $cnpj = $_POST["cnpj"];
+    $nome = $_POST["nome-instituicao"];
+    $endereco = $_POST["endereco"];
+    $senha = $_POST["senha"];
+    $senhaCheck = $_POST["senha-check"];
 
-$cnpj = $_POST["cnpj"];
-$nome = $_POST["nome-instituicao"];
-$endereco = $_POST["endereco"];
-$senha = $_POST["senha"];
-$senhaCheck = $_POST["senha-check"];
+    if ($senha != $senhaCheck) {
+        die("As senhas não correspondem.");
+    }   
 
-if ($senha != $senhaCheck) {
-    die("A senha está diferente.");
-}   
+    $host = "localhost";
+    $dbname = "OrgEvento";
+    $username = "root";
+    $password = "";
+            
+    $conn = mysqli_connect(hostname: $host, username: $username, password: $password, database: $dbname);
+            
+    if (mysqli_connect_errno()) {
+        die("Erro de conexão: " . mysqli_connect_error());
+    }           
+            
+    $sql = "INSERT INTO Instituicoes (cnpj, nome, endereco, senha) VALUES (?, ?, ?, ?)";
 
-$host = "localhost";
-$dbname = "OrgEvento";
-$username = "root";
-$password = "";
-        
-$conn = mysqli_connect(hostname: $host,
-                       username: $username,
-                       password: $password,
-                       database: $dbname);
-        
-if (mysqli_connect_errno()) {
-    die("Connection error: " . mysqli_connect_error());
-}           
-        
-$sql = "INSERT INTO Instituicoes (cnpj, nome, endereco, senha)
-        VALUES (?, ?, ?, ?)";
+    $stmt = mysqli_stmt_init($conn);
 
-$stmt = mysqli_stmt_init($conn);
+    if ( ! mysqli_stmt_prepare($stmt, $sql)) {
+        die(mysqli_error($conn));
+    }
 
-if ( ! mysqli_stmt_prepare($stmt, $sql)) {
- 
-    die(mysqli_error($conn));
-}
+    mysqli_stmt_bind_param($stmt, "ssii", $cnpj, $nome, $endereco, $senha);
 
-mysqli_stmt_bind_param($stmt, "ssii",
-                       $cnpj,
-                       $nome,
-                       $endereco,
-                       $senha);
+    mysqli_stmt_execute($stmt);
 
-mysqli_stmt_execute($stmt);
-
-echo "Instituicao cadastrada.";
+    echo "Instituição cadastrada.";
+?>
